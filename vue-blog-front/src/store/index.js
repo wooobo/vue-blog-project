74cfcfb866bbf0f410ff428800b9ce090e9f9f93
강태화 <aogora2431@gmail.com>
2019-12-07 22:22:30 +0900

    - mapState - mapGetters 추가

--- vue-blog-front/src/store/index.js
+++ vue-blog-front/src/store/index.js
@@ -5,10 +5,25 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    items: [
+      {
+        title: '제목 1',
+        content: '내용 1',
+        isActive: false,
+      },
+      {
+        title: '제목 2',
+        content: '내용 2',
+        isActive: true,
+      },
+    ],
   },
   mutations: {
   },
   actions: {
+  },
+  getters: {
+    ActiveItems: state => state.items.filter(item => item.isActive),
   },
   modules: {
   },
--- vue-blog-front/src/views/Home.vue
+++ vue-blog-front/src/views/Home.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="home">
-    HOME
+    <div>
+      <p>state로 가져온 값</p>
+      {{items}}
+    </div>
+    <br />
+    <div>
+      <p>mapGetters로 가져온 값</p>
+      <div>
+        {{ActiveItems}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { mapState, mapGetters } from 'vuex'; // vuex에서 "mapState"를 가져옵니다.
 
 export default {
   name: 'home',
+  computed: {
+    ...mapState(['items']), // mapState에 items를 등록합니다. 여기서 items는 ./src/store/index.js에서 state에 등록한 object 입니다.
+    ...mapGetters(['ActiveItems']),
+  },
 };
 </script>
 
